cf1c8f622b3974df2cb07bbd3bdb8077368dfb7e
Aachal <aachalmakode900@gmail.com>
2021-08-03 17:15:25 -0400

    Added: Emp wage in array

--- JS_Basics/EmployeeWage.js
+++ JS_Basics/EmployeeWage.js
@@ -1,27 +1,36 @@
-console.log("Program For Employee Wage Problem");
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORK_DAYS = 20;
-const MAX_HRS_IN_MONTH =100;
-let empHrs = 0;
-let work_Days = 0;
-function getWorkingHours(empCheck){
-    switch (empCheck) {
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0
+let empDailyWageArray = new Array();
+
+function calDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++
+    let empcheck =  Math.floor(Math.random()* 10) % 3;
+    let empHrs = getWorkingHours(empcheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArray.push(calDailyWage(empHrs));
+}
+
+function getWorkingHours(empcheck) {
+    switch(empcheck) {
         case IS_PART_TIME:
             return PART_TIME_HOURS;
         case IS_FULL_TIME:
             return FULL_TIME_HOURS;
         default:
-            return 0;
+        return 0; 
     }
 }
-while (empHrs <= MAX_HRS_IN_MONTH && work_Days<NUM_OF_WORK_DAYS){
-    work_Days++;
-    let empCheck = Math.floor(Math.random() *10) %3;
-    empHrs += getWorkingHours(empCheck);
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hours: "+empHrs+"\nEmployee Wage: "+empWage);
+let empWage = calDailyWage(totalEmpHrs);
+console.log(empDailyWageArray);
+console.log("Total Days:" +totalWorkingDays +"total Emp Hrs: "+totalEmpHrs +"Employee Wage:" +empWage);
